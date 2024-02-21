ca485606efa3dda571e456485a7deba8dc2840ff
sobreratas <111362320+sobreratas@users.noreply.github.com>
2024-02-21 12:27:27 -0500

    add intersection observer

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-function changeHamburgerBackground(){
-    const hamburger = document.querySelector(".hamburger-container")
-    let scrollValue = window.scrollY;
-    if(scrollValue > 900){
-        hamburger.style.backgroundColor = "black";
-    } else {
-        hamburger.style.backgroundColor = "transparent";
-    }
-}
+// function changeHamburgerBackground(){
+//     const hamburger = document.querySelector(".hamburger-container")
+//     let scrollValue = window.scrollY;
+//     if(scrollValue > 900){
+//         hamburger.style.backgroundColor = "black";
+//     } else {
+//         hamburger.style.backgroundColor = "transparent";
+//     }
+// }
 
-window.addEventListener('scroll', changeHamburgerBackground);
+// window.addEventListener('scroll', changeHamburgerBackground);
+
+//MIDDLE CARD CLASS INTERSECTION OBSERVER 
+const middleCards = document.querySelectorAll(".middle-card");
+
+const middleObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+        if (entry.isIntersecting) middleObserver.unobserve(entry.target)
+    })
+},
+    {
+        threshold: .5,
+    })
+
+middleCards.forEach(card => {
+    middleObserver.observe(card)
+})
